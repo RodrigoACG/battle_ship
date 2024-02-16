36c28d4b98b67b9fc4d7569e543342518751e7f0
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-15 18:52:28 -0700

    Fire upon methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,7 @@ class Cell
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
+        @fired_upon = false
     end
 
     def empty?
@@ -13,4 +14,15 @@ class Cell
     def place_ship(ship_object)
         @ship = ship_object
     end
+
+    def fired_upon? 
+        @fired_upon 
+    end
+
+    def fire_upon
+        @fired_upon = true 
+        @ship.hit unless @ship == nil
+    end
+
+    
 end
