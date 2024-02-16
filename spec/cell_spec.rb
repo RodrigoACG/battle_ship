0fb75f7c1b759d492e2ff93ad669ce07dfc6119b
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-15 18:31:23 -0700

    test for cell attributes

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -9,8 +9,14 @@ RSpec.describe Cell do
    expect(@cell).to be_a(Cell)
   end
 
-  it 'attributes of a cell' do 
-    expect(@cell.coordinate).to eq("B4")
+  describe '#cell attributes' do
+    it 'coordinate of a cell' do 
+      expect(@cell.coordinate).to eq("B4")
+    end
 
+    it 'ship' do 
+      expect(@cell.ship).to eq(nil)
+    end
   end
+
 end
