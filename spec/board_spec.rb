e7ed53a421fa2434c4523d6edd17e61fd8036c17
Steddman Bell <steddfast@gmail.com>
2024-02-16 17:10:32 -0800

    Completes intial board class and board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+RSpec.describe Board do
+  before(:each) do
+    @board_1 = Board.new
+    @grid = @board_1.cells.keys
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@board_1).to be_instance_of Board
+    end
+
+    it 'has grid of cells' do
+        expect(@board_1.cells.keys).to eq(@grid)
+    end
+  end
+
+
+end
