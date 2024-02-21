911aa79bb522171e6c538cca0f548e7e5f874941
Steddman Bell <steddfast@gmail.com>
2024-02-21 13:57:47 -0800

    Fixes the conditional in the render method to player ship and overall flow of if block

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,48 +1,40 @@
 class Cell
-    attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship
 
-    def initialize(coordinate)
-        @coordinate = coordinate
-        @ship = nil
-        @fired_upon = false
-    end
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
 
-    def cell_empty?
-        @ship == nil
-    end
+  def cell_empty?
+    @ship == nil
+  end
 
-    def place_ship(ship_object)
-        @ship = ship_object
-    end
+  def place_ship(ship_object)
+    @ship = ship_object
+  end
 
-    def fired_upon? 
-        @fired_upon 
-    end
+  def fired_upon? 
+    @fired_upon 
+  end
 
-    def fire_upon
-        @fired_upon = true 
-        @ship.hit unless @ship == nil
-    end
+  def fire_upon
+    @fired_upon = true 
+    @ship.hit unless @ship == nil
+  end
 
-    def render(ship_cond = nil) 
-        # require 'pry'; binding.pry
-        if ship_cond == true 
-            
-            if  @fired_upon == false && @ship != nil
-                return " S"
-            elsif @fired_upon == true && @ship.sunk? == true
-                return " X"
-            elsif @fired_upon == true && @ship.sunk? == false
-                return " H"
-            end      
-        end
-        # require 'pry'; binding.pry
-        if 
-            @fired_upon == true && @ship == nil
-            " M"
-        else
-            " ."   
-        end
-        
+  def render(player_ship = nil) 
+    if player_ship == true && @fired_upon == false && @ship != nil
+      " S"   
+    elsif  @fired_upon == true && @ship == nil
+      " M"
+    elsif @fired_upon == true && @ship.sunk? == true
+      " X"
+    elsif @fired_upon == true && @ship.sunk? == false
+      " H"
+    else
+      " ."   
     end
+  end
 end
